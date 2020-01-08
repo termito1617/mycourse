486e765b441da2ae8057c7f1789fa0209a3ea2cd
Vladlen Parechyn <vladlen.parechyn@sap.com>
2020-01-08 17:07:26 +0200

    Create initial structure of Spring MVC app

---
+++ src/main/java/com/mycourse/config/AnnotationWebAppInitializer.java
@@ -0,0 +1,21 @@
+package com.mycourse.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class AnnotationWebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(AppConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
